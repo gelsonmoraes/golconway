cb4c5c859f414b18c57858e5b0fee62d9e21651a
Gelson Moraes <gelsonluizmoraes@gmail.com>
2024-02-22 09:35:58 -0300

    Implementa a funcionalidade de mudar o estado da célula ao clicar na mesma.

--- main.py
+++ main.py
@@ -5,42 +5,62 @@ import pygame
 pygame.init()
 
 # Definindo algumas constantes relativas à aparencia da grade.
-PRETO = (0, 0, 0)
-CINZA = (128, 128)
-AZUL = (66, 133, 250)
-LARGURA, ALTURA = 600, 600
-TAMANHO_CELULA = 15
-LARGURA_GRADE = LARGURA / TAMANHO_CELULA
-ALTURA_GRADE = ALTURA / TAMANHO_CELULA
+PRETO = (13, 13, 13)
+CINZA = (65, 65, 65)
+AZUL = (0, 30, 212)
+VERMELHO = (255, 5, 5)
+LARGURA, ALTURA = 660, 660
+TAMANHO_CELULA = 33
+LARGURA_GRADE = LARGURA // TAMANHO_CELULA
+ALTURA_GRADE = ALTURA // TAMANHO_CELULA
 FPS = 60
 
 tela = pygame.display.set_mode((LARGURA, ALTURA))
-
 tempo = pygame.time.Clock()
 
 # Desenhando a grade
-
-
 def desenhando_grade(posicoes):
-    for linha in range(ALTURA_GRADE):
-        pygame.draw.line(tela, PRETO(0, linha * TAMANHO_CELULA), (LARGURA, linha * TAMANHO_CELULA))
+
+    for posicao in posicoes:
+        coluna, linha = posicao
+        superior_esquerdo = (coluna * TAMANHO_CELULA, linha * TAMANHO_CELULA)
+        pygame.draw.rect(tela, AZUL, (*superior_esquerdo,TAMANHO_CELULA, TAMANHO_CELULA))
+
+    for linha in range(int(ALTURA_GRADE)):
+        pygame.draw.line(tela, PRETO, (0, linha * TAMANHO_CELULA),
+                         (LARGURA, linha * TAMANHO_CELULA))
+
+    for coluna in range(int(ALTURA_GRADE)):
+        pygame.draw.line(tela, PRETO, (coluna * TAMANHO_CELULA,
+                         0), (coluna * TAMANHO_CELULA, ALTURA))
 
 
 def main():
     rodando = True
-    clock = pygame.time.Clock()
     posicoes = set()
-    #screen = pygame.Surface(15, 15)
 
     while rodando:
-        clock.tick(FPS)
+        tempo.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 rodando = False
                 
-                
-    #screen.fill(CINZA)                
-    desenhando_grade(posicoes)       
+            #Implementa a funcionalidade de mudar o estado da célula ao clicar na mesma.    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                coluna = x // TAMANHO_CELULA
+                linha = y // TAMANHO_CELULA
+                pos = (coluna, linha)
+                if pos in posicoes:
+                    posicoes.remove(pos)
+                else:
+                    posicoes.add(pos)
+
+        tela.fill(CINZA)
+        desenhando_grade(posicoes)
+        pygame.display.update()
+
     pygame.quit()
 
 
